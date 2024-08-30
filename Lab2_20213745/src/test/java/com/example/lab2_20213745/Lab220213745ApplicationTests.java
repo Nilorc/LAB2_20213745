08bfd689443c5e05dc0dba4dba04d162042fcafa
NiloRC <145509554+Nilorc@users.noreply.github.com>
2024-08-29 19:25:32 -0500

    Add files via upload
    Creación del repositorio

---
+++ Lab2_20213745/src/test/java/com/example/lab2_20213745/Lab220213745ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20213745;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220213745ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
